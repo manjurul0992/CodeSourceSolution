59ded39dc7e4b6e6287ec9e5d40860741f20c580
manjurul0992 <monjurulislam327@gmail.com>
2024-04-27 02:48:12 +0600

    complete index page

---
+++ CodeSourceSolution/Models/Format.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeSourceSolution.Models
+{
+    public class Format
+    {
+        public Format()
+        {
+            this.SeriesEntries = new HashSet<SeriesEntry>();
+        }
+
+        public int FormatId { get; set; }
+        [Required, StringLength(100), Display(Name = "Format Name")]
+        public string? FormatName { get; set; }
+
+        public virtual ICollection<SeriesEntry> SeriesEntries { get; set; }
+    }
+}
---
+++ CodeSourceSolution/Models/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+
+namespace CodeSourceSolution.Models
+{
+    public class Player
+    {
+        public Player()
+        {
+            SeriesEntries = new HashSet<SeriesEntry>();
+        }
+        public int PlayerId { get; set; }
+        [Required, StringLength(100), Display(Name = "Player Name")]
+        public string? PlayerName { get; set; }
+        [Required, Column(TypeName = "date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? DateOfBirth { get; set; }
+        [Required, StringLength(100), Display(Name = "Phone Number")]
+        public string? Phone { get; set; }
+        [Required, StringLength(100), Display(Name = "Picture")]
+        public string? Picture { get; set; }
+        public bool? MaritalStatus { get; set; }
+        public virtual ICollection<SeriesEntry> SeriesEntries { get; set; }
+    }
+}
